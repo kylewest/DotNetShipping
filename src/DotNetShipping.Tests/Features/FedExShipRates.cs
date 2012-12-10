3ac0541f891d611d82a12b4b2e4f69a9bc422d35
Brett Allred <brettallred@gmail.com>
2012-12-10 11:41:33 -0700

    Updated .gitignore Added Tests Project with x-unit

---
+++ src/DotNetShipping.Tests/Features/FedExShipRates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace DotNetShipping.Tests.Features
+{
+    public class FedExShipRates
+    {
+
+        [Fact]
+        public void FedExReturnsRates()
+        {
+
+            
+
+
+        }
+    }
+}
