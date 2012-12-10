3acf622cc102ac2e433363218fd4628d9f40d32f
Brett Allred <brettallred@gmail.com>
2012-12-10 13:39:34 -0700

    Fedex Can Get Rates Test

--- src/DotNetShipping.Tests/Features/FedExShipRates.cs
+++ src/DotNetShipping.Tests/Features/FedExShipRates.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 using Xunit;
 
@@ -10,14 +6,32 @@ namespace DotNetShipping.Tests.Features
 {
     public class FedExShipRates
     {
+        /*  FedEx Rate Lookup
+         *  https://www.fedex.com/ratefinder/home?cc=US&language=en&locId=express
+         */
+
+        #region Methods
 
         [Fact]
         public void FedExReturnsRates()
         {
+            RateManager rateManager = RateManagerFactory.Build();
 
-            
+            var from = new Address("Salt Lake City", "UT", "84119", "US");
+            var to = new Address("Beverly Hills", "CA", "90210", "US");
+            var package = new Package(12, 12, 12, 12, 0);
 
+            Shipment r = rateManager.GetRates(from, to, package);
 
+            Assert.NotNull(r);
+            Assert.True(r.rates.Any());
+
+            foreach (var rate in r.rates)
+            {
+                Assert.True(rate.TotalCharges > 0);
+            }
         }
+
+        #endregion
     }
 }
