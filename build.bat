1b4d3f182598ca69bf47aaef7bd4d3c74a5be9ca
Kyle West <kswest@gmail.com>
2013-09-03 11:04:40 -0400

    new versioning numbers

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-tools\NAnt\NAnt.exe -buildfile:nant.build %*
+SET version=0
+FOR /F "tokens=1" %%a IN ('git rev-list master') DO SET /A version+=1
+tools\NAnt\NAnt.exe -buildfile:nant.build -D:version.revision=%version% %*
