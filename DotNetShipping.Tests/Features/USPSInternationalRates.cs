01fe739f0158c8457bf09a073a046a300511bcb9
Kyle West <kswest@gmail.com>
2015-05-29 11:59:54 -0400

    update USPS test to use country code

--- DotNetShipping.Tests/Features/USPSInternationalRates.cs
+++ DotNetShipping.Tests/Features/USPSInternationalRates.cs
@@ -11,38 +11,38 @@ namespace DotNetShipping.Tests.Features
 {
     public class USPSInternationalRates
     {
-        private readonly Address DomesticAddress1;
-        private readonly Address DomesticAddress2;
-        private readonly Address InternationalAddress1;
-        private readonly Address InternationalAddress2;
-        private readonly Package Package1;
-        private readonly Package Package2;
-        private readonly string USPSUserId;
+        private readonly Address _domesticAddress1;
+        private readonly Address _domesticAddress2;
+        private readonly Address _internationalAddress1;
+        private readonly Address _internationalAddress2;
+        private readonly Package _package1;
+        private readonly Package _package2;
+        private readonly string _uspsUserId;
 
         public USPSInternationalRates()
         {
-            DomesticAddress1 = new Address("278 Buckley Jones Road", "", "", "Cleveland", "MS", "38732", "US");
-            DomesticAddress2 = new Address("One Microsoft Way", "", "", "Redmond", "WA", "98052", "US");
-            InternationalAddress1 = new Address("Jubail", "Jubail", "31951", "Saudi Arabia"); //has limited intl services available
-            InternationalAddress2 = new Address("80-100 Victoria St", "", "", "London SW1E 5JL", "", "", "United Kingdom");
+            _domesticAddress1 = new Address("278 Buckley Jones Road", "", "", "Cleveland", "MS", "38732", "US");
+            _domesticAddress2 = new Address("One Microsoft Way", "", "", "Redmond", "WA", "98052", "US");
+            _internationalAddress1 = new Address("Jubail", "Jubail", "31951", "SA"); //has limited intl services available
+            _internationalAddress2 = new Address("80-100 Victoria St", "", "", "London", "", "SW1E 5JL", "GB");
 
-            Package1 = new Package(4, 4, 4, 5, 0);
-            Package2 = new Package(6, 6, 6, 5, 100);
+            _package1 = new Package(4, 4, 4, 5, 0);
+            _package2 = new Package(6, 6, 6, 5, 100);
 
-            USPSUserId = ConfigurationManager.AppSettings["USPSUserId"];
+            _uspsUserId = ConfigurationManager.AppSettings["USPSUserId"];
         }
 
         [Fact]
         public void USPS_Intl_Returns_Multiple_Rates_When_Using_Multiple_Packages_For_All_Services_And_Multiple_Packages()
         {
             var packages = new List<Package>();
-            packages.Add(Package1);
-            packages.Add(Package2);
+            packages.Add(_package1);
+            packages.Add(_package2);
 
             var rateManager = new RateManager();
-            rateManager.AddProvider(new USPSInternationalProvider(USPSUserId));
+            rateManager.AddProvider(new USPSInternationalProvider(_uspsUserId));
 
-            var response = rateManager.GetRates(DomesticAddress1, InternationalAddress2, packages);
+            var response = rateManager.GetRates(_domesticAddress1, _internationalAddress2, packages);
 
             Debug.WriteLine(string.Format("Rates returned: {0}", response.Rates.Any() ? response.Rates.Count.ToString() : "0"));
 
@@ -63,9 +63,9 @@ namespace DotNetShipping.Tests.Features
         public void USPS_Intl_Returns_Multiple_Rates_When_Using_Valid_Addresses_For_All_Services()
         {
             var rateManager = new RateManager();
-            rateManager.AddProvider(new USPSInternationalProvider(USPSUserId));
+            rateManager.AddProvider(new USPSInternationalProvider(_uspsUserId));
 
-            var response = rateManager.GetRates(DomesticAddress1, InternationalAddress2, Package1);
+            var response = rateManager.GetRates(_domesticAddress1, _internationalAddress2, _package1);
 
             Debug.WriteLine(string.Format("Rates returned: {0}", response.Rates.Any() ? response.Rates.Count.ToString() : "0"));
 
@@ -86,9 +86,9 @@ namespace DotNetShipping.Tests.Features
         public void USPS_Intl_Returns_No_Rates_When_Using_Invalid_Addresses_For_All_Services()
         {
             var rateManager = new RateManager();
-            rateManager.AddProvider(new USPSInternationalProvider(USPSUserId));
+            rateManager.AddProvider(new USPSInternationalProvider(_uspsUserId));
 
-            var response = rateManager.GetRates(DomesticAddress1, DomesticAddress2, Package1);
+            var response = rateManager.GetRates(_domesticAddress1, _domesticAddress2, _package1);
 
             Debug.WriteLine(string.Format("Rates returned: {0}", response.Rates.Any() ? response.Rates.Count.ToString() : "0"));
 
@@ -102,9 +102,9 @@ namespace DotNetShipping.Tests.Features
             //can't rate intl with a domestic address
 
             var rateManager = new RateManager();
-            rateManager.AddProvider(new USPSInternationalProvider(USPSUserId, "Priority Mail International"));
+            rateManager.AddProvider(new USPSInternationalProvider(_uspsUserId, "Priority Mail International"));
 
-            var response = rateManager.GetRates(DomesticAddress1, DomesticAddress2, Package1);
+            var response = rateManager.GetRates(_domesticAddress1, _domesticAddress2, _package1);
 
             Debug.WriteLine(string.Format("Rates returned: {0}", response.Rates.Any() ? response.Rates.Count.ToString() : "0"));
 
@@ -116,9 +116,9 @@ namespace DotNetShipping.Tests.Features
         public void USPS_Intl_Returns_Single_Rate_When_Using_Valid_Addresses_For_Single_Service()
         {
             var rateManager = new RateManager();
-            rateManager.AddProvider(new USPSInternationalProvider(USPSUserId, "Priority Mail International"));
+            rateManager.AddProvider(new USPSInternationalProvider(_uspsUserId, "Priority Mail International"));
 
-            var response = rateManager.GetRates(DomesticAddress1, InternationalAddress1, Package1);
+            var response = rateManager.GetRates(_domesticAddress1, _internationalAddress1, _package1);
 
             Debug.WriteLine(string.Format("Rates returned: {0}", response.Rates.Any() ? response.Rates.Count.ToString() : "0"));
 
